9adfed4be5a625f9118dc7e578e94ef2187d4718
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2026-03-10 10:50:59 +0400

    adding SessionVM.cs file for ViewModels directory

---
+++ src/PLDMS.PL/Areas/Mentor/ViewModels/SessionVM.cs
@@ -0,0 +1,14 @@
+using PLDMS.BL.DTOs;
+using PLDMS.Core.Enums;
+
+namespace PLDMS.PL.Areas.Mentor.ViewModels;
+
+public record SessionVM
+{
+    public ICollection<SessionTableItemDTO> Sessions { get; set; } = [];
+    public int TotalCount { get; set; }
+    public int CurrentPage { get; set; }
+    public int PageSize { get; set; }
+    public string? Search { get; set; }
+    public SessionStatus? Status { get; set; }
+}
